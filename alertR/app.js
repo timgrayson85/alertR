b0eb87c20be870ea175f497597a65f3dd4947d4b
Tim <timgrayson85@gmail.com>
2017-06-21 17:31:03 +0100

    Moved JavaScript from HTML to static file

--- alertR/app.js
+++ alertR/app.js
@@ -1,11 +1,15 @@
-﻿var app = require('express')();
-var http = require('http').Server(app);
+﻿var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 
 app.get('/', function (req, res) {
-        res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
+
 });
+
+app.use(express.static('./public'));
 
 
 io.on('connection', function (socket) {
---
+++ alertR/public/functions.js
@@ -0,0 +1,39 @@
+﻿var socket = io();
+
+function addAppEvent(name) {
+    socket.emit('addApplication', name);
+}
+
+socket.on('application-added', function addApplication(name) {
+    // Add the clicked application to the list of my Apps.
+    var ul = document.getElementById("myApps");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(name));
+    //li.addEventListener("click", function () { alert(name);})
+    ul.appendChild(li);
+});
+
+
+function searchApps() {
+    // Declare variables.
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
+socket.on('some event', function (msg) {
+    alert("joined room");
+});
