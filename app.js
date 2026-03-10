48134ccfea170fa0d070212c9eb8f074095ca3ba
Tim <timgrayson85@gmail.com>
2026-03-10 08:26:25 +0000

    ♻️ Replace MySQL with LowDB for lightweight storage
    - Remove mysql dependency
    - Add lowdb for JSON-based file storage
    - Create db-setup.js with LowDB implementation
    - Rewrite app.js to use async queries
    - No external database server required
    - Data stored in data/db.json
    
    This simplifies the stack for prototype/demo purposes.
    SQLite compilation issues on ARM led to LowDB choice.

--- app.js
+++ app.js
@@ -1,95 +1,114 @@
-﻿var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var con = require('./mysql-setup');
-var ip = require('ip');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(http);
+const { initDb, queries } = require('./db-setup');
+const ip = require('ip');
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-// Make JavaScript and CSS files available.
-app.use(express.static('./public'));
-
-
-io.on('connection', function (socket) {
-
-    // Get a unique identifer for this client.
-    var ip = socket.conn.remoteAddress;
-    console.log('User: ' + ip + ' has connected');
-
-    // Create a new room for this client.
-    socket.join(ip + '-room');
-
-    // Listen for an 'add-Application' event then push it to all sockets assigned to the client. 
-    // This is necessary in case the user has multiple tabs or browsers open.
-    socket.on('add-application', function (app) {
-        io.to(ip + '-room').emit('application-added', app);
-        console.log('User ' + ip + ' added: ' + app + ' to their apps');
+// Initialise database before starting server
+initDb().then(() => {
+    
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/index.html');
     });
 
-    socket.on('remove-application', function (app) {
-        io.to(ip + '-room').emit('application-removed', app);
-        console.log('User ' + ip + ' removed: ' + app + ' from their apps');
-    });
+    // Make JavaScript and CSS files available.
+    app.use(express.static('./public'));
 
-    socket.on('remove-subscription', function (app) {
-        io.to(ip + '-room').emit('subscription-removed', app);
-        console.log('User ' + ip + ' removed: ' + app + ' from their subscriptions');
-    });   
+    io.on('connection', function (socket) {
 
-    // Listen for an 'add-subscription' event then push it to all sockets assigned to the client.
-    // This is necessary in case the user has multiple tabs or browsers open.
-    socket.on('add-subscription', function (app) {
+        // Get a unique identifier for this client.
+        const clientIp = socket.conn.remoteAddress;
+        console.log('User: ' + clientIp + ' has connected');
 
-        //Get the current status of this applicaion.
-        con.query("select h.ApplicationName, h.AlertName, DATE_FORMAT(h.Date, '%m/%d/%Y %H:%i:%S') as Date, Description from mydb.history h inner join " +
-            "(select ApplicationName, max(Date) as MaxDate from mydb.history group by ApplicationName) " +
-            "an on h.ApplicationName = an.ApplicationName and h.Date = an.MaxDate and an.ApplicationName = '" + app + "'", function (err, result, fields) {
-                if (err) throw err;
-                var alertLevel = result[0].AlertName;
-                var alertDate = result[0].Date;
-                var alertMessage = result[0].Description;
+        // Create a new room for this client.
+        socket.join(clientIp + '-room');
 
-                io.to(ip + '-room').emit('subscription-added', { Name: app, AlertLevel: alertLevel, AlertDate: alertDate, AlertMessage: alertMessage  });
-                console.log('User ' + ip + ' subscribed to: ' + app);
-            });
-    });
-
-    // Alert all clients that an alert has been raised on an application.
-    socket.on('alert-raised', function (json) {
-
-        // Write alert to database.
-        con.query("INSERT INTO History (ApplicationName, AlertName, Date, Description) VALUES ('" + json.Name + "','" + json.AlertLevel + "', NOW(), '" + json.AlertMessage + "')", function (err, result) {
-            if (err) throw err;
-            console.log("Number of records inserted: " + result.affectedRows);
+        // Listen for an 'add-Application' event then push it to all sockets assigned to the client.
+        // This is necessary in case the user has multiple tabs or browsers open.
+        socket.on('add-application', function (app) {
+            io.to(clientIp + '-room').emit('application-added', app);
+            console.log('User ' + clientIp + ' added: ' + app + ' to their apps');
         });
 
-        io.emit('alert-raised', json);
-        console.log(json.AlertLevel + ' alert has been raised on ' + json.Name);
-
-    });
-
-    socket.on('get-application-stats', function (app) {
-        con.query("select Date, count(*) from history where ApplicationName = '" + app + "' group by MONTH(Date) order by Date desc", function (err, results) {
-            if  (err) throw err;
-
-            io.to(ip + '-room').emit('stats-returned', results);
-            alert(results);
+        socket.on('remove-application', function (app) {
+            io.to(clientIp + '-room').emit('application-removed', app);
+            console.log('User ' + clientIp + ' removed: ' + app + ' from their apps');
         });
+
+        socket.on('remove-subscription', function (app) {
+            io.to(clientIp + '-room').emit('subscription-removed', app);
+            console.log('User ' + clientIp + ' removed: ' + app + ' from their subscriptions');
+        });
+
+        // Listen for an 'add-subscription' event then push it to all sockets assigned to the client.
+        // This is necessary in case the user has multiple tabs or browsers open.
+        socket.on('add-subscription', async function (appName) {
+            try {
+                const result = queries.getLatestAlert(appName);
+                
+                if (result) {
+                    const alertDate = new Date(result.date).toLocaleString();
+                    io.to(clientIp + '-room').emit('subscription-added', {
+                        Name: result.applicationName,
+                        AlertLevel: result.alertName,
+                        AlertDate: alertDate,
+                        AlertMessage: result.description
+                    });
+                    console.log('User ' + clientIp + ' subscribed to: ' + appName);
+                } else {
+                    // No history for this app, subscribe with default OK status
+                    io.to(clientIp + '-room').emit('subscription-added', {
+                        Name: appName,
+                        AlertLevel: 'OK',
+                        AlertDate: new Date().toLocaleString(),
+                        AlertMessage: 'No alerts'
+                    });
+                    console.log('User ' + clientIp + ' subscribed to: ' + appName + ' (no history)');
+                }
+            } catch (err) {
+                console.error('Error subscribing to application:', err);
+            }
+        });
+
+        // Alert all clients that an alert has been raised on an application.
+        socket.on('alert-raised', async function (json) {
+            try {
+                // Write alert to database.
+                await queries.addAlert(json.Name, json.AlertLevel, json.AlertMessage);
+                console.log('Alert recorded for ' + json.Name);
+                
+                io.emit('alert-raised', json);
+                console.log(json.AlertLevel + ' alert has been raised on ' + json.Name);
+            } catch (err) {
+                console.error('Error recording alert:', err);
+            }
+        });
+
+        socket.on('get-application-stats', function (appName) {
+            try {
+                const results = queries.getAppStats(appName);
+                io.to(clientIp + '-room').emit('stats-returned', results);
+            } catch (err) {
+                console.error('Error getting stats:', err);
+            }
+        });
+
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+
     });
 
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
+    http.listen(port, function () {
+        // Put a friendly message on the terminal
+        console.log('Server running at http://' + ip.address() + ':' + port + '/');
     });
 
+}).catch(err => {
+    console.error('Failed to initialise database:', err);
+    process.exit(1);
 });
-
-http.listen(port, function () {
-    // Put a friendly message on the terminal
-    console.log('Server running at http://' + ip.address() +':' + port + '/');
-});
-
